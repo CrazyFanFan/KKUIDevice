6dc4bd92e7104bb14f9c3bc1202eea6547fb7a41
Crazy凡 <kongkk@fxiaoke.com>
2017-10-31 21:05:36 +0800

    init project

---
+++ KKUIDevice/KKUIDevice.h
@@ -0,0 +1,19 @@
+//
+//  KKUIDevice.h
+//  KKUIDevice
+//
+//  Created by Crazy凡 on 31/10/2017.
+//  Copyright © 2017 Crazy凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKUIDevice.
+FOUNDATION_EXPORT double KKUIDeviceVersionNumber;
+
+//! Project version string for KKUIDevice.
+FOUNDATION_EXPORT const unsigned char KKUIDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKUIDevice/PublicHeader.h>
+
+
