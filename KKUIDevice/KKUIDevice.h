1d0a01d0765c1ea13e2f2ae3915238ebf5835009
Crazy凡 <kongkk@fxiaoke.com>
2017-10-31 21:15:47 +0800

    add UIDevice (KKUIDevice)

--- KKUIDevice/KKUIDevice.h
+++ KKUIDevice/KKUIDevice.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char KKUIDeviceVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <KKUIDevice/PublicHeader.h>
 
+@interface UIDevice (KKUIDevice)
 
+@end
---
+++ KKUIDevice/KKUIDevice.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+KKUIDevice.m
+//  KKUIDevice
+//
+//  Created by Crazy凡 on 31/10/2017.
+//  Copyright © 2017 Crazy凡. All rights reserved.
+//
+
+#import "KKUIDevice.h"
+
+@implementation UIDevice (KKUIDevice)
+
+@end
