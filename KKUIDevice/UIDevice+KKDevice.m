76df655f0a1fcb8f05fc25bee14d7830fbb3a5ea
Crazy凡 <kongkk@fxiaoke.com>
2017-11-03 15:18:01 +0800

    change to category

--- KKUIDevice/UIDevice+KKDevice.m
+++ KKUIDevice/UIDevice+KKDevice.m
@@ -1,28 +1,36 @@
 //
-//  UIDevice+KKUIDevice.m
+//  UIDevice+KKDevice.m
 //  KKUIDevice
 //
-//  Created by Crazy凡 on 31/10/2017.
+//  Created by Crazy凡 on 03/11/2017.
 //  Copyright © 2017 Crazy凡. All rights reserved.
 //
 
-#import "KKUIDevice.h"
+#import "UIDevice+KKDevice.h"
 #import <sys/utsname.h>
 
 // ADFA
 #import <AdSupport/AdSupport.h>
 
+// define info
 #define BUNDLE_NAME @"KKUIDevice"
 #define PLIST_NAME @"HardwareModelMap.plist"
 
-@implementation UIDevice (KKUIDevice)
 
+@implementation UIDevice (KKDevice)
+
+/**
+ @return "Hardware strings"(eg. iPhone10,1)
+ */
 - (NSString *)KKHardware {
     struct utsname systemInfo;
     uname(&systemInfo);
     return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
+/**
+ @return device model (device name, e.g. iPhone6/iPhone 6 Plus),return "unknown device" when it's not define
+ */
 - (NSString *)KKModel {
     NSString *plistPath = [[[NSBundle mainBundle] pathForResource:BUNDLE_NAME ofType:@"bundle"] stringByAppendingPathComponent:PLIST_NAME];
 
@@ -36,6 +44,9 @@
     return model;
 }
 
+/**
+ @return IDFA
+ */
 - (NSString *)KKAdvertisingIdentifier {
     return [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
 }
