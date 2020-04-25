731c45299d6682b00d38f3c83de291ad95fd1272
Crazy凡 <827799383@qq.com>
2020-04-25 15:19:43 +0800

    Replace the string with enum

--- Example/KKUIDeviceDemo/ViewController.swift
+++ Example/KKUIDeviceDemo/ViewController.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2018年 C凡. All rights reserved.
 //
 
-import UIKit
 import KKUIDevice
+import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ ModelType.swift
@@ -0,0 +1,69 @@
+//
+// ModelType.swift
+// KKUIDevice
+//
+// Created by Crazy凡 on 2020/4/25.
+//
+
+import Foundation
+
+public enum ModelType: String {
+    case unknown = "unknown device"
+
+    case mac_32 = "Mac(32bit)"
+    case mac_64 = "Mac(64bit)"
+
+    case simulator = "Simulator"
+    
+    case iPad1ST = "iPad (1ST)"
+    case iPad2 = "iPad 2"
+    case iPadMini = "iPad Mini"
+    case iPad3RD = "iPad (3RD)"
+    case iPad4ST = "iPad (4ST)"
+    case iPadAir = "iPad Air"
+    case iPadMini2 = "iPad Mini 2"
+    case iPadMini3 = "iPad Mini 3"
+    case iPadMini4 = "iPad Mini 4"
+    case iPadAir2 = "iPad Air 2"
+    case iPadPro9_7_inch = "iPad Pro 9.7-inch"
+    case iPadPro12_9_inch1ST = "iPad Pro 12.9-inch (1ST)"
+    case iPad2017 = "iPad (2017)"
+    case iPadPro12_9_inch2ND = "iPad Pro 12.9-inch (2ND)"
+    case iPadPro10_5_inch = "iPad Pro 10.5-inch"
+    case iPad2018 = "iPad (2018)"
+    case iPadPro11_inch = "iPad Pro 11-inch"
+    case iPadPro12_9_inch3RD = "iPad Pro 12.9-inch(3RD)"
+    case iPodTouch1ST = "iPod Touch (1ST)"
+    case iPodTouch2ND = "iPod Touch (2ND)"
+    case iPodTouch3RD = "iPod Touch (3RD)"
+    case iPodTouch4TH = "iPod Touch (4TH)"
+    case iPodTouch5TH = "iPod Touch (5TH)"
+    case iPodTouch6TH = "iPod Touch (6TH)"
+    case iPhone = "iPhone"
+    case iPhone3G = "iPhone (3G)"
+    case iPhone3GS = "iPhone (3GS)"
+    case iPhone4 = "iPhone 4"
+    case iPhone4S = "iPhone 4S"
+    case iPhone5 = "iPhone 5"
+    case iPhone5c = "iPhone 5c"
+    case iPhone5s = "iPhone 5s"
+    case iPhone6Plus = "iPhone 6 Plus"
+    case iPhone6 = "iPhone 6"
+    case iPhone6S = "iPhone 6S"
+    case iPhone6SPlus = "iPhone 6S Plus"
+    case iPhoneSE = "iPhone SE"
+    case iPhone7CDMA = "iPhone 7 (CDMA)"
+    case iPhone7PlusCDMA = "iPhone 7 Plus (CDMA)"
+    case iPhone7 = "iPhone 7"
+    case iPhone7Plus = "iPhone 7 Plus"
+    case iPhone8 = "iPhone 8"
+    case iPhone8Plus = "iPhone 8 Plus"
+    case iPhoneX = "iPhone X"
+    case iPhoneXS = "iPhone XS"
+    case iPhoneXSMax = "iPhone XS Max"
+    case iPhoneXR = "iPhone XR"
+    case iPhone11 = "iPhone 11"
+    case iPhone11Pro = "iPhone 11 Pro"
+    case iPhone11ProMax = "iPhone 11 Pro Max"
+    case iPhoneSE2ND = "iPhone SE (2ND)"
+}
